c3c3a1a0fbb04f2e794388a8ea64efabc90cd68a
Emmanuel Nwakanma <emmynetro@gmail.com>
2022-10-28 21:07:23 -0400

    deployment #1

--- deploy_on_pythonanywhere/deploy_on_pythonanywhere/urls.py
+++ deploy_on_pythonanywhere/deploy_on_pythonanywhere/urls.py
@@ -1,7 +1,7 @@
-"""housing URL Configuration
+"""deploy_on_pythonanywhere URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from housing import views
 
 urlpatterns = [
-    path('home/', admin.site.urls),
     path('admin/', admin.site.urls),
-    path('housing/', views.housing_list),
-    path('housing/<int:id>', views.housing_detail),
 ]
