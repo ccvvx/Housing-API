f87fc24042177c44b7d3b82c0e00b1a9e36c9d46
Emmanuel Nwakanma <emmynetro@gmail.com>
2023-04-12 08:51:23 -0400

    static files

--- housing/settings.py
+++ housing/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', "0.0.0.0:$PORT",'housing-api-production.up.railway.app']
 
 
 # Application definition
@@ -133,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
